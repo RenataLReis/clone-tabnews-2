c9d7d93544cba1112fcca37ac847988f074aa733
RenataLReis <92191735+RenataLReis@users.noreply.github.com>
2025-12-10 18:15:24 +0000

    adiciona comandos `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews-2
+
 Repositório paradidático para acompanhamento do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Agora uma declaração que estará sempre no ar, como meu amor por vc ^^</h1>
+  return (
+    <h1>
+      Agora uma declaração que estará sempre no ar, como meu amor por vc ^^
+    </h1>
+  );
 }
 
 export default Home;
