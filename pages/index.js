78c3f75385fa63aef753df4b28ee2585db12005b
RenataLReis <92191735+RenataLReis@users.noreply.github.com>
2025-12-11 21:51:12 +0000

    fix: ajusta mensagem da página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <h1>
-      Agora uma declaração que estará sempre no ar, como meu amor por vc ^^
+      BusTab: a aba do seu navegador com informações sobre mobilidade urbana
     </h1>
   );
 }
