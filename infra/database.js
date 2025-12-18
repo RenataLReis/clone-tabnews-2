57aff2d64ec45d5b95b67ae8c0d1408555e06a7c
RenataLReis <92191735+RenataLReis@users.noreply.github.com>
2025-12-18 13:40:58 +0000

    makes `infra/database.js` more robust in error handling

--- infra/database.js
+++ infra/database.js
@@ -9,13 +9,20 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
-  await client.connect();
+  console.log("Variáveis de ambiente do Postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
+    await client.connect();
     return await client.query(queryObject);
   } catch (err) {
     console.error(err);
-    return null;
+    throw error;
   } finally {
     await client.end();
   }
